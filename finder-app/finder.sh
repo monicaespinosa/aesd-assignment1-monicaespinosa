3d8be3353a6cb09513f929b1baf92b89f3a62987
Samanta <monica.spinosa21@gmail.com>
2026-04-01 01:15:54 +0200

    assignment 1 is done

---
+++ finder-app/finder.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Write a shell script finder-app/finder.sh as described below:
+#    1) Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, referred to below as filesdir;
+#       the second argument is a text string which will be searched within these files, referred to below as searchstr
+#    2) Exits with return value 1 error and print statements if any of the parameters above were not specified
+#    3) Exits with return value 1 error and print statements if filesdir does not represent a directory on the filesystem
+#    4) Prints a message "The number of files are X and the number of matching lines are Y" where X is the number of files in the directory and 
+#       all subdirectories and Y is the number of matching lines found in respective files, where a matching line refers to a line which contains 
+#       searchstr (and may also contain additional content).
+
+# (1)
+filesdir=$1
+searchstr=$2
+
+# (2)
+if [ -z $filesdir ] || [ -z $searchstr ]
+then
+    echo "one or both of the parameters (filesdir and searchstr) are not specified."
+    echo "hint: finder.sh <filesdir> <searchstr>"
+    exit 1
+fi
+
+# (3)
+if [ ! -d $filesdir ]
+then
+    echo "$filesdir is not a directory"
+    exit 1
+fi
+
+# (4)
+# count the number of files at given directory. 
+# $ - checks number of arg(;find - if directory exist; -type f - if sstandar file type; wc -l write new lines) 
+numfiles=$(find $filesdir -type f | wc -l)
+# count the matches in the found files
+matches=$(grep -r $searchstr $filesdir | wc -l)
+
+echo "The number of files are $numfiles and the number of matching lines are $matches"
---
+++ finder-app/writer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Write a shell script finder-app/writer.sh as described below
+#    1) Accepts the following arguments: the first argument is a full path to a file (including filename) on the filesystem, referred to below as writefile; 
+#       the second argument is a text string which will be written within this file, referred to below as writestr
+#    2) Exits with value 1 error and print statements if any of the arguments above were not specified
+#    3) Creates a new file with name and path writefile with content writestr, overwriting any existing file and creating the path if it doesn’t exist. Exits 
+#       with value 1 and error print statement if the file could not be created.
+
+writefile=$1
+writestr=$2
+
+if [ -z $writefile ] || [ -z $writestr ]
+then
+    echo "one or both of the needed arguments was not specified."
+    echo "syntax hint: write.sh <writefile> <writestr>"
+    exit 1
+fi
+
+writepath=$(dirname $writefile)
+mkdir -p $writepath
+
+if [ $? -ne 0 ]
+then
+    echo "Couldn't create path. Possibility: path already exist"
+    exit 1
+fi
+
+echo $writestr > $writefile
+
+if [ $? -ne 0 ]
+then
+    echo "Couldn't create file $writepath"
+    exit 1
+fi
